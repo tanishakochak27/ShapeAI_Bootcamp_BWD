40041545f1f012881baccbd70c72ab35a30bff4c
Tanisha Kochak <82718280+tanishakochak27@users.noreply.github.com>
2021-06-05 22:46:47 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+  return ( 
+  <div className ="note">
+    <h1>Javascript and React.js</h1>
+    <p>This was an amazing bootcamp taken up by shaurya sir.we coverred everything from sratch including React,HTML.</p>
+  </div>
+  );
+}
+
+export default Note;
